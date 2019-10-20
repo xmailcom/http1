e70a34d02b9df863316aee8df897391d72c5a72b
xmailcom <55943921+xmailcom@users.noreply.github.com>
2019-10-20 14:14:19 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+var request = require('request');
+var express = require('express');
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('port', (process.env.PORT || 5000));
+app.set('host', (process.env.HOST || '0.0.0.0'));
+
+app.post('/x', function(req, res) {
+    var url = req.body['url'];
+    var headers = req.body['headers'];
+    var method = req.body['method'];
+    var body = req.body['body'];
+    console.log(url);
+    console.log(headers);
+    console.log(method);
+    console.log(body);
+    request({
+        url: url,
+        headers: headers,
+        method: method,
+        body: body,
+    }, function(error, response, body) {
+        res.header("Access-Control-Allow-Origin", req.headers.origin);
+        res.header("Access-Control-Allow-Credentials", "true");
+        res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+        res.send({headers: response.headers, body: body});
+    });
+});
+
+app.listen(app.get('port'), app.get('host'), function() {
+    console.log("Node app is running at " + app.get('host') + ":" + app.get('port'));
+});
